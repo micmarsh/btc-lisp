dbb2f42a0d0e669941917002df0dd448b5e205ad
micmarsh <mike@marsh.pw>
2015-10-30 20:14:29 -0400

    old version of bitcoinj causing trouble

--- test/btc_lisp/test/compiler.clj
+++ test/btc_lisp/test/compiler.clj
@@ -1,10 +1,10 @@
-(ns btc-lisp.compiler-test
+(ns btc-lisp.test.compiler
   (:require [clojure.test :refer :all]
             [btc-lisp.compiler :refer [compile*]]
+            [btc-lisp.test.helpers.gen.code :refer :all]
             [btc-lisp.test.helpers
              [script-eval :as s]
              [clojurify :as c]]
-            [btc-lisp.test.helpers.gen.code :refer :all]
             [clojure.test.check
              [clojure-test :refer [defspec]]
              [properties :as prop]]))
@@ -20,8 +20,7 @@
   {:clojure (eval (c/clojurify btc-lisp-code))
    :btc-lisp (s/eval-opcodes (compile* (str btc-lisp-code)))})
 
-(defspec test-compiler-valid-output
-  {:num-tests 50 :seed 1445808966733}
+(defspec test-compiler-valid-output 100
   (prop/for-all
    [btc-lisp-code gen-primitive-expr]
    (output= (eval (c/clojurify btc-lisp-code))
@@ -37,5 +36,5 @@
   ;; diff, which ur not accounting for in clj-mock.
 
   ;; Have to decide if this is even a problem, too
-  
+
   )
